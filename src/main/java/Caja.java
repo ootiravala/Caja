ba4b1524804097f25280e55bc1fea5b22b41f198
coste <coste@DESKTOP-45JMSAU.lan>
2020-09-27 16:51:08 +0200

    Commit Message

---
+++ src/main/java/Caja.java
@@ -0,0 +1,22 @@
+
+public class Caja {
+
+	int ancho;
+	int alto;
+	int profundo;
+
+	public Caja() {
+
+	}
+
+	public Caja(int ancho, int alto, int profundo) {
+		this.ancho = ancho;
+		this.alto = alto;
+		this.profundo = profundo;
+	}
+
+	public int calcularVolumen(int ancho, int alto, int profundo) {
+		return ancho * alto * profundo;
+	}
+
+}
---
+++ src/main/java/PruebaCaja.java
@@ -0,0 +1,17 @@
+
+public class PruebaCaja {
+
+	public PruebaCaja() {
+
+	}
+
+	public static void main(String[] args) {
+
+
+		Caja caja = new Caja();
+
+		System.out.println("El volumen de la caja es; " + caja.calcularVolumen(3, 2, 6));
+
+	}
+
+}
